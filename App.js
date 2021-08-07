25c0d757fdbf95bb9f4ff2531c8e5c8801a01055
Samuel Ng <ngsamu4@gmail.com>
2021-08-07 16:18:52 -0400

    Implemented basic 2 screen app.

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.homeText}>Welcome to TFT Stat Checker!</Text>
+      <Button
+        title='Screen2'
+        onPress={() => navigation.navigate('Screen2')}
+      ></Button>
     </View>
+  );
+};
+
+const Screen2 = () => {
+  return (
+    <View>
+      <Text>Screen2</Text>
+    </View>
+  );
+};
+
+export default function App() {
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Screen2' component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  homeText: {
+    color: '#fff',
   },
 });
